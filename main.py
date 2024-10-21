eed66af6ee1e2bb652d0c7f616f7fae2d42e2fa0
lagairogo <70363482+lagairogo@users.noreply.github.com>
2024-10-21 07:27:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from tempfile import NamedTemporaryFile
 
+import base64
 import streamlit as st
 from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 from tools import ImageCaptionTool, ObjectDetectionTool
+from openai import OpenAI
+import logging
 
+OPENAI_API_KEY=''
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
 ##############################
 ### initialize agent #########
@@ -20,9 +29,9 @@ conversational_memory = ConversationBufferWindowMemory(
 )
 
 llm = ChatOpenAI(
-    openai_api_key='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',
+    openai_api_key='',
     temperature=0,
-    model_name="gpt-3.5-turbo"
+    model_name="gpt-4o"
 )
 
 agent = initialize_agent(
@@ -36,7 +45,7 @@ agent = initialize_agent(
 )
 
 # set title
-st.title('Ask a question to an image')
+st.title('Architecture assistant')
 
 # set header
 st.header("Please upload an image")
@@ -57,9 +66,45 @@ if file:
     with NamedTemporaryFile(dir='.') as f:
         f.write(file.getbuffer())
         image_path = f.name
+        logging.warning(image_path)
 
-        # write agent response
+        base64_image = encode_image(image_path)
+
+        client = OpenAI(api_key=OPENAI_API_KEY)
         if user_question and user_question != "":
             with st.spinner(text="In progress..."):
-                response = agent.run('{}, this is the image path: {}'.format(user_question, image_path))
-                st.write(response)
+                response = client.chat.completions.create(
+                    model="gpt-4o",
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "image_url",
+                                    "image_url": { "url": f"data:image/jpeg;base64,{base64_image}"
+                                }
+                            },
+                            {
+                                "type": "text",
+                               "text": user_question
+                            }
+                            ]
+                        }
+                    ],
+                    temperature=1,
+                    max_tokens=2048,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    response_format={
+                        "type": "text"
+                    }
+                )
+                st.write(response.choices[0].message.content)
+
+        # write agent response
+        #if user_question and user_question != "":
+         #   with st.spinner(text="In progress..."):
+          #      response = agent.run('{}, this is the image path: {}'.format(user_question, image_path))
+           #     st.write(response)
+
